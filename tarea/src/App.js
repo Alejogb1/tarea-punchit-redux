4f5bac6058879de8c601704f321b621c5b1466f0
Alejogb1 <alejogarciag1@gmail.com>
2021-02-03 21:20:08 -0300

    -
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- tarea/src/App.js
+++ tarea/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom"
+import {Provider} from "react-redux"
+import store from "./redux/store"
+import Todo from "./Pages/todo"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Route path="/todos" component={Todo}></Route>
+        <Redirect to="/todos"/>
+      </Router>
+    </Provider>
   );
 }
 
