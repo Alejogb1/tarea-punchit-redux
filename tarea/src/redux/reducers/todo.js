fb124b2ee124e2e58ec211988f128f486245fbc5
Alejogb1 <alejogarciag1@gmail.com>
2021-02-03 21:19:49 -0300

    Update todo.js
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- tarea/src/redux/reducers/todo.js
+++ tarea/src/redux/reducers/todo.js
@@ -1,23 +1,26 @@
 import {actions} from "./../actions/todo"
 
-const initalState = {
+const initialState = {
+    autor: "Tu mama",
     todos: [
         {id: 1, description: "Esto es una tarea", status: false}
     ]
 }
-export default function todoReducer(state = initalState){
-    switch(action.type) {
-        case actions.ADD_TODO:
-            return {
-                ...state,
-                todos:  [...state.todos , action.payload.todo ]
-            }
-        case actions.DELETE_TODO:
-            return {
-                ...state,
-                todos:  state.todos.filter(todo => todo.id !== action.payload.id),
-            };
-        defualt:
-            return state
+export default function todoReducer(state = initialState, action) {
+    console.log(action);
+    switch (action.type) {
+      case actions.ADD_TODO:
+        return {
+          ...state,
+          todos: [action.payload.todo, ...state.todos],
+        };
+  
+      case actions.DELETE_TODO:
+        return {
+          ...state,
+          todos: state.todos.filter((todo) => todo.id !== action.payload.id),
+        };
+      default:
+        return state;
     }
-}
+  }
