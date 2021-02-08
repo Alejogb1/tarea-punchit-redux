e384e69c39f1b4241437ad0d80ab943616762c5a
Alejogb1 <alejogarciag1@gmail.com>
2021-02-08 12:34:38 -0300

    camelCase fix
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- tarea/src/App.js
+++ tarea/src/App.js
@@ -2,10 +2,12 @@ import {BrowserRouter as Router, Route, Redirect} from "react-router-dom"
 import {Provider} from "react-redux"
 import store from "./redux/store"
 import Todo from "./Pages/todo"
+import AddTodo from "./components/addTodo"
 function App() {
   return (
     <Provider store={store}>
       <Router>
+        <AddTodo/>
         <Route path="/todos" component={Todo}></Route>
         <Redirect to="/todos"/>
       </Router>
