6275877b395a5542f18f53b226acd08136af9568
Alejogb1 <alejogarciag1@gmail.com>
2021-02-17 20:57:26 -0300

    -
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- tarea/src/components/addTodo.js
+++ tarea/src/components/addTodo.js
@@ -2,19 +2,18 @@ import {Col, Form} from "react-bootstrap"
 import {connect} from "react-redux"
 import {nanoid} from "nanoid/non-secure"
 const addTodo = ({dispatch}) =>  {
-    function addTask (e) {
+    console.log("DISPATCH:", dispatch)
+    /*function addTask (e) {
         e.preventDefault();
         const [description] = e.target
         const obj = {
-            id: nanoid(),
-            description: description.value,
-            status: false
+            []
         }
         dispatch({type: "ADD_TODO", payload: {todo: obj} })
-    }
+    }*/
     return (
         <Col>
-            <Form onSubmit={addTask}>
+            <Form>
                 <input placeholder="Escibe una nueva tarea"></input>
                 <button type="submit"></button>
             </Form>
