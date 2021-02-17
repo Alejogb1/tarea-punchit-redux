83cfc930f42fca083aadc08d12be286b24c08347
Alejogb1 <alejogarciag1@gmail.com>
2021-02-17 20:57:17 -0300

    Update todo.js
    Co-Authored-By: Alejo Garcia <52103963+alejogb1@users.noreply.github.com>

--- tarea/src/Pages/todo.js
+++ tarea/src/Pages/todo.js
@@ -5,17 +5,21 @@ import { actions } from "../redux/actions/todo"
 
 const Todo = ({dispatch, data: stockData}) => {
     console.log("DATA: ", stockData.data)
-    /*const deleteActivity =  (id) => {
-        dispatch({ type: actions.DELETE_TODO, payload: {id}})
-    }*/
+    const agregarStock =  (id, ticker, precio, titulo) => {
+        
+    dispatch({ type: actions.ADD_STOCK, payload: {id, ticker, stockPrice: precio, company: titulo}})
+    }
     return (<>
                 {
+                    
                     stockData.data.length ?
                     stockData.data.map((stock) => (
                         <div>
+                            {console.log("XD: "+ stock.company)},
                             <h3>{stock.company}</h3>
                             <p>{stock.ticker}</p>
                             <h5>{stock.stockPrice}</h5>
+                            <button onClick={() => agregarStock(stock.id, stock.ticker, stock.stockPrice, stock.company)}>Agregar stock</button>
                         </div>
                     ))
                     : <h4>Ups! No hay stocks</h4>
